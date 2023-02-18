33bbd44111a64f3a95d0518cea214b637119e35c
luckyhope1 <geoffryoduor92@gmail.com>
2023-02-18 12:19:05 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,11 +20,11 @@ printf("last digit of %d is %d and is greater than 5\n", n, n % 10);
 }
 else if ((n % 10) < 6 && (n % 10) != 0)
 {
-printf(" last digit of %d is %d and is less than 6 and not 6\n", n, n % 10);
+printf(" last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
 }
 else
 {
-printf("last digit of %d is %d is less than 0\n", n, n % 10);
+printf("last digit of %d is %d is 0\n", n, n % 10);
 }
 return (0);
 }
