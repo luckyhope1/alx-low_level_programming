e500f7f47d62b1c65b129f497398b31901c483ad
luckyhope1 <geoffryoduor92@gmail.com>
2023-02-18 22:27:14 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ *
+ * Return: o Success
+ */
+int main(void)
+{
+	char a = '0';
+
+	while (a <= '9')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+return (0);
+}
