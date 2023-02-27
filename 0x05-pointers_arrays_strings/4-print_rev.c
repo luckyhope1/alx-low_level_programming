5b9829a185d390a8684a619c967e19789484201c
luckyhope1 <geoffryoduor92@gmail.com>
2023-02-27 04:53:23 -0800

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+	int longi = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		longi++;
+	s++;
+	}
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
+s--;
+}
+_putchar('\n');
+}
