3396c123bbbf0481d00291f18b211b891e2be703
luckyhope1 <geoffryoduor@gmail.com>
2023-07-27 14:08:04 -0700

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -17,16 +17,16 @@ def island_perimeter(grid):
         - One cell is a square with side length 1.
         - Grid cells are connected horizontally/vertically (not diagonally).
         - Grid is rectangular, width and height don't exceed 100.
-        - Grid is completely surrounded by water, and there is one island (or nothing).
-        - The island doesn't have "lakes" (water inside that isn't connected to the water around the island).
+        - Grid is completely surrounded by water, and there is one island
+        (or nothing).
+        - The island doesn't have "lakes" (water inside that isn't connected to
+        the water around the island).
     """
     perimeter = 0
 
     for row in range(len(grid)):
         for col in range(len(grid[0])):
             if grid[row][col] == 1:
-                # Check the four sides of the current cell
-                # If a side is water or out of the grid boundaries, add to the perimeter
                 if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
                 if row == len(grid) - 1 or grid[row + 1][col] == 0:
